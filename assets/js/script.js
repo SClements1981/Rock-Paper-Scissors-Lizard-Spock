96817c45314ced34a52622d67e5a8ed9daae6ab7
Steven Clements <sgclements1981@gmail.com>
2024-05-13 16:21:36 +0000

    add basic structure to js

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// Rock paper scissor lizard spock
+
+const choices = ["rock", "paper", "scissor", "lizard", "spock"];
+
+
+function runGame(playerChoice) {
+
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementHumanScore() {
+
+}
+
+function incrementComputerScore() {
+
+}
