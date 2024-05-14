925034f251aa259358a358469e3d42d13c708b76
Steven Clements <sgclements1981@gmail.com>
2024-05-14 11:39:32 +0000

    changed check winner function and added eventlistener to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,57 @@
 // Rock paper scissor lizard spock
+console.log("hello");
+ 
+const choices = ["rock", "paper", "scissor", "lizard", "spock"];
 
-const choice = ["rock", "paper", "scissor", "lizard", "spock"];
-const playerDisplay = document.getElementById("player-image");
-const computerDisplay = document.getElementById("computer-image");
-const result = document.getElementsByClassName("result");
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
 
-function runGame(playerChoice) {
-
-    const computerChoice = choice[Math.floor(Math.random() * 5)];
-    let result = "";
-
-    if (playerChoice === computerChoice) {
-        result = "Draw!"
+    for (let button of buttons){
+        button.addEventListener("click", function() {
+        if playerSelections = this.Attribute("aria-label");
+            alert(`You clicked ${playerSelections}`);
+        })
     }
-    else {
+})
+
+
+function runGame(){
+
+}
+
+function computerChoice(){
+    const choice = choices[Math.floor(Math.random() * choices.length)];
+    return choice;
+}
+
+function playerChoice(){
+
+}
+
+function checkWinner(){
+    if (playerChoice === computerChoice){
+        result = "Draw!";
+    }
+    else{
         switch(playerChoice){
-            case "rock" : result = (computerChoice === "scissors", "lizard") ? "Winner!" : "Loser!"
+            case "rock": result = (computerChoice === "scissor" || "lizard") ? "Winner!" : "Loser!"
             break;
-            case "paper" : result = (computerChoice === "rock", "spock") ? "Winner!" : "Loser!"
+            case "paper": result = (computerChoice === "rock" || "spock") ? "Winner!" : "Loser!"
             break;
-            case "scissor" : result = (computerChoice === "paper", "lizard") ? "Winner!" : "Loser!"
+            case "scissor": result = (computerChoice === "paper" || "lizard") ? "Winner!" : "Loser!"
             break;
-            case "lizard" : result = (computerChoice === "paper", "spock") ? "Winner!" : "Loser!"
+            case "lizard": result = (computerChoice === "paper" || "spock") ? "Winner!" : "Loser!"
             break;
-            case "spock" : result = (computerChoice === "rock", "scissor") ? "Winner!" : "Loser!"
+            case "spock": result = (computerChoice === "rock" || "scissor") ? "Winner!" : "Loser!"
             break;
         }
     }
+}
 
-    playerDisplay.textContent = `player-image: ${playerChoice}`;
-    computerDisplay.textContent = `computer-image: ${computerChoice}`;
-    result.textContent = result;
-
-function calculateCorrectAnswer() {
+function calculateCorrectAnswer(){
 
 }
 
-function incrementHumanScore() {
-
-}
-
-function incrementComputerScore() {
+function incrementScore(){
 
 }
