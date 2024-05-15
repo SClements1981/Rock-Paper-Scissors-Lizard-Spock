5b310319dbb305ecdbf7a350d5a0af0a546cb021
Steven Clements <sgclements1981@gmail.com>
2024-05-15 19:53:34 +0000

    combined checkwinner function and computer random choice function to fix the results output.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,84 +1,71 @@
 // Rock paper scissor lizard spock
 
 /** DOM */
-const choices = ["rock", "paper", "scissor", "lizard", "spock"];
+const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
 
+// Functionality for player and computer scores
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
 
-/** Reset game  - possible first to 5 and add reset button*/
 
-
-/** Player and computer scores */
-let playerScore = document.getElementById("player-score").innerText;
-let computerScore = document.getElementById("computer-score").innerText;
-
-/** Computer random choice */
-function generateRandomChoice() {
-    const choice = choices[Math.floor(Math.random() * choices.length)];
-    return choice;
-}
-
-/** Check winner */
+// Functions checkWinner for computer and player choices
 function checkWinner(playerChoice) {
-    const computerChoice = generateRandomChoice();
-    let result;
+    // Computer random choice
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
     if (playerChoice === computerChoice) {
-        result = "draw";
-        console.log(result);
+        result = "IT'S A TIE!";
+    }
+    else {
+        switch (playerChoice) {
+            case "rock":
+                result = (computerChoice === "paper" || computerChoice === "spock") ? "lose" : "win";
+                break;
+            case "paper":
+                result = (computerChoice === "scissor" || computerChoice === "lizard") ? "lose" : "win";
+                break;
+            case "scissor":
+                result = (computerChoice === "rock" || computerChoice === "spock") ? "lose" : "win";
+                break;
+            case "lizard":
+                result = (computerChoice === "rock" || computerChoice === "scissor") ? "lose" : "win";
+                break;
+            case "spock":
+                result = (computerChoice === "paper" || computerChoice === "lizard") ? "lose" : "win";
+                break;
         }
-        else {
-    switch (playerChoice) {
-        case "rock":
-            result = (computerChoice === "paper" || computerChoice === "spock") ? "lose" : "win";
-            break;
-        case "paper":
-            result = (computerChoice === "scissor" || computerChoice === "lizard") ? "lose" : "win";
-            break;
-        case "scissor":
-            result = (computerChoice === "rock" || computerChoice === "spock") ? "lose" : "win";
-            break;
-        case "lizard":
-            result = (computerChoice === "rock" || computerChoice === "scissor") ? "lose" : "win";
-            break;
-        case "spock":
-            result = (computerChoice === "paper" || computerChoice === "lizard") ? "lose" : "win";
-            break;
-        }
-         if (result === "win") {
+        if (result === "win") {
             incrementScorePlayer();
         }
         if (result === "lose") {
             incrementScoreComputer();
         }
-
-    /** Show player and computer selection */
-    playerDisplay.textContent = `You: ${playerChoice}`;
-    computerDisplay.textContent = `Computer: ${computerChoice}`;
-    resultDisplay.textContent = result;
     }
+        // Player and computer choices displayed on screen
+        playerDisplay.textContent = `You: ${playerChoice}`;
+        computerDisplay.textContent = `Computer: ${computerChoice}`;
+    
+        // Result of playGame displayed on screen
+        resultDisplay.textContent = result;
 }
-
-
-/** Player score */
+    
+    /** Player score */
 function incrementScorePlayer() {
     let previousScore = parseInt(playerScore);
     let updatedScore = previousScore + 1;
     playerScore = updatedScore;
-    document.getElementById("player-score").innerText = updatedScore;
-}
+    document.getElementById("playerScoreDisplay").innerText = updatedScore;
+    }
 
-/** Computer score */
-function incrementScoreComputer() {
+    /** Computer score */
+    function incrementScoreComputer() {
     let previousComputerScore = parseInt(computerScore);
     let updatedComputerScore = previousComputerScore + 1;
     computerScore = updatedComputerScore;
-    document.getElementById("computer-score").innerText = updatedComputerScore;
-}
-
-
-/**  
- * ReadMe
- * Reset game
- */
+    document.getElementById("computerScoreDisplay").innerText = updatedComputerScore;
+    }
